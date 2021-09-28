e912b8ebe12dfcbd66e7255d0441ea40461d3c18
Rushil Shah <rushil180101@gmail.com>
2021-09-28 21:52:32 +0530

    Hide Django SECRET_KEY.

--- src/social_network/settings.py
+++ src/social_network/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os.path
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c@56$!&pp@op)+7$n-#g9tiqyy5@vcyx*%z=u-!(g*0rs=*)rz'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
