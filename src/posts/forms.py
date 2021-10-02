6f87beace73a2e1bf9558962ebb1f231fd0cc2e2
Rushil Shah <rushil180101@gmail.com>
2021-10-02 17:01:07 +0530

    Created CommentModelForm and created the logic to submit and view comments.
    - The logic to handle new Post form submissions and new Comment form
      submissions is currently defined in the same function all_posts_view()
      in `views.py`.
    
    - Single page contains 2 forms (for Post and Comment) which are
      differentiated in the logic by using different names for submit
      buttons.

--- src/posts/forms.py
+++ src/posts/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import *
 
 
@@ -6,3 +7,20 @@ class PostModelForm(ModelForm):
     class Meta:
         model = Post
         fields = ['image', 'content']
+
+
+class CommentModelForm(ModelForm):
+    # Edit the css and widgets while displaying the form.
+    body = forms.CharField(
+        label='',
+        widget=forms.Textarea(
+            attrs={
+                'placeholder': 'Add a comment...',
+                'rows': 2,
+            }
+        )
+    )
+
+    class Meta:
+        model = Comment
+        fields = ['body']
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import *
 from profiles.models import Profile
-from .forms import PostModelForm
+from .forms import *
 
 
 def all_posts_view(request):
@@ -11,22 +11,40 @@ def all_posts_view(request):
     # Form for creating new post by the logged in user.
     new_post_form = PostModelForm()
 
-    if request.method == 'POST':
-        new_post_form = PostModelForm(request.POST, request.FILES)
-        if new_post_form.is_valid():
-            # Before saving the new post, we want to assign its author as the current logged in user.
-            # Thus, to prevent automatic save, we pass the parameter commit=False as follows.
-            instance = new_post_form.save(commit=False)
-            instance.author = logged_in_user_profile
-            instance.save()
+    # Form for creating new comments on the posts by the logged in user.
+    new_comment_form = CommentModelForm()
 
-            # Create new empty form once the old post form is submitted and saved.
-            new_post_form = PostModelForm()
+    if request.method == 'POST':
+        # Handle Post form submission
+        if 'submit_post_form' in request.POST:
+            new_post_form = PostModelForm(request.POST, request.FILES)
+            if new_post_form.is_valid():
+                # Before saving the new post, we want to assign its author as the current logged in user.
+                # Thus, to prevent automatic save, we pass the parameter commit=False as follows.
+                instance = new_post_form.save(commit=False)
+                instance.author = logged_in_user_profile
+                instance.save()
+
+                # Create new empty form once the old post form is submitted and saved.
+                new_post_form = PostModelForm()
+
+        # Handle Comment form submission
+        if 'submit_comment_form' in request.POST:
+            new_comment_form = CommentModelForm(request.POST)
+            if new_comment_form.is_valid():
+                instance = new_comment_form.save(commit=False)
+                instance.user = logged_in_user_profile
+                instance.post = Post.objects.get(id=request.POST.get('post_id'))
+                instance.save()
+
+                # Create new empty form once the old one is submitted.
+                new_comment_form = CommentModelForm()
 
     context = {
         'posts_qs': posts_qs,
         'profile': logged_in_user_profile,
         'new_post_form': new_post_form,
+        'new_comment_form': new_comment_form,
     }
     return render(request, 'posts/main.html', context)
 # Note: The above function all_posts_view() can also be implemented with Django ListView
