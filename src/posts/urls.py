eef9bcf13649bb4c37613e57d9d6f70b5210b72a
Rushil Shah <rushil180101@gmail.com>
2021-10-03 13:54:42 +0530

    First commit for test-approach-1 branch. Added test comment.

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -5,3 +5,5 @@ urlpatterns = [
     path('', all_posts_view, name='main-posts-view'),
     path('like-unlike-post', like_unlike_post, name='like-unlike-post'),
 ]
+
+# Test comment for different branch.
