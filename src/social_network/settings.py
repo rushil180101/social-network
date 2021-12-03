529124153db82109aa1687899f9ae8d356ce22f5
Rushil Shah <rushil180101@gmail.com>
2021-12-03 17:03:51 +0530

    Postgres Database connection

--- src/social_network/settings.py
+++ src/social_network/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'social_network.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'the-social-network-db-1',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
